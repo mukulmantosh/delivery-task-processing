e5ddee894637e60f1723683ebaa9c4746bbb2f24
Mukul Mantosh <mukulmantosh@yahoo.com>
2019-03-28 06:33:21 +0530

    Initial Commit.

--- urban_piper/urls.py
+++ urban_piper/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name="login"),
     path('logout/', views.LogoutView.as_view(), name="logout"),
     path('admin/', admin.site.urls),
-    path('store/', include('store.urls')),
+    path('store/', login_required(include('store.urls'))),
     path('api/', include('api.v1.urls')),
 
 ]
